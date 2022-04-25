5888d8a4983469cd0c0355633013ef0f11edb52a
Jon Ramm <jrgbone@gmail.com>
2022-04-25 14:07:52 -0600

    cleanup, comments

--- client/showboat.py
+++ client/showboat.py
@@ -34,11 +34,7 @@ class UI(QtWidgets.QMainWindow):
         self.bandBioHeader_2 = self.findChild(QtWidgets.QLabel, "bandBioHeader_2")
         self.homeScrollArea = self.findChild(QtWidgets.QScrollArea, "homeScrollArea")
         self.homeScrollArea.setWidgetResizable(True)
-        # self.tourDatesHomeHeader = self.findChild(QtWidgets.QLabel, "tourDatesHomeHeader")
-        # self.homeTourDatesLabel = self.findChild(QtWidgets.QLabel, "homeTourDatesLabel")
-        
         self.bandSearchLineEdit = self.findChild(QtWidgets.QLineEdit, "bandSearchLineEdit")
-
         self.lastSearchButton = self.findChild(QtWidgets.QPushButton, "lastSearchButton")
         self.searchButton = self.findChild(QtWidgets.QPushButton, "searchButton")
 
@@ -46,72 +42,56 @@ class UI(QtWidgets.QMainWindow):
         self.tourDatesDateLabel = self.findChild(QtWidgets.QLabel, "tourDatesDateLabel")
         self.tourDatesVenueLabel = self.findChild(QtWidgets.QLabel, "tourDatesVenueLabel")
         self.tourDatesCityLabel = self.findChild(QtWidgets.QLabel, "tourDatesCityLabel")
-        # self.tourDatesStateLabel = self.findChild(QtWidgets.QLabel, "tourDatesStateLabel")
         self.tourDatesTicketInfoLabel = self.findChild(QtWidgets.QLabel, "tourDatesTicketInfoLabel")
         self.tourDatesDateListLabel = self.findChild(QtWidgets.QLabel, "tourDatesDateListLabel")
         self.tourDatesVenueListLabel = self.findChild(QtWidgets.QLabel, "tourDatesVenueListLabel")
         self.tourDatesCityListLabel = self.findChild(QtWidgets.QLabel, "tourDatesCityListLabel")
-        # self.tourDatesStateListLabel = self.findChild(QtWidgets.QLabel, "tourDatesStateListLabel")
         self.tourDatesTicketListLabel = self.findChild(QtWidgets.QLabel, "tourDatesTicketListLabel")
 
         # Map
         self.mapMapContainer = self.findChild(QWebEngineView, "mapMapContainer")
-        # self.mapHomeCityLineEdit = self.findChild(QtWidgets.QLineEdit, "mapHomeCityLineEdit")
-        # self.mapSetRadiusSlider = self.findChild(QtWidgets.QSlider, "mapSetRadiusSlider")
-        # self.mapSetCityButton = self.findChild(QtWidgets.QPushButton, "mapSetCityButton")
-        # self.mapDateDisplayLabel = self.findChild(QtWidgets.QLabel, "mapDateDisplayLabel")
-        # self.mapVenueDisplayLabel = self.findChild(QtWidgets.QLabel, "mapVenueDisplayLabel")
-        # self.mapCityDisplayLabel = self.findChild(QtWidgets.QLabel, "mapCityDisplayLabel")
-        # self.mapTicketsDisplayLabel = self.findChild(QtWidgets.QLabel, "mapTicketsDisplayLabel")
-
-        coordinates = (39.70495909177235, -101.79370097549975)
+        coordinates = (27.03992362079509, -22.920434372492934)
         self.map = folium.Map(
             title='',
-            zoom_start=3,
+            zoom_start=2,
             location=coordinates
         )
-
         # save map data to data object
         data = io.BytesIO()
         self.map.save(data, close_file=False)
-
         self.mapMapContainer.setHtml(data.getvalue().decode())
 
         # Video
-
         self.video0 = self.findChild(QWebEngineView, "video0")
         self.video1 = self.findChild(QWebEngineView, "video1")
         self.video2 = self.findChild(QWebEngineView, "video2")
-
+        # put video widgets in a dict for easy iteration
         self.video_dict = {
             "video0": self.video0,
             "video1": self.video1,
             "video2": self.video2
         }
-
         self.videoSeeMoreButton = self.findChild(QtWidgets.QPushButton, "videoSeeMoreButton")
 
         # Functionality
-
         self.searchButton.clicked.connect(self.artist_search)
-        # self.mapSetCityButton.clicked.connect(self.city_search_test)
 
-    
         # show the app
         self.show()
 
     # Functions
 
     def artist_search(self):
+        # get search string from search bar
         value = self.bandSearchLineEdit.text()
         obj = {"artist_search": value}
-
-        screen = QtWidgets.QSplashScreen()
-        screen.showMessage('Loading...')
-        screen.setGeometry(QtCore.QRect(650,400,100,100))
-        screen.show()
+        # create and show loading screen
+        splash_screen = QtWidgets.QSplashScreen()
+        splash_screen.showMessage('Loading...')
+        splash_screen.setGeometry(QtCore.QRect(650,400,100,100))
+        splash_screen.show()
         app.processEvents()
-
+        # call artist-search endpoint
         response = requests.post('http://127.0.0.1:5000/artist-search', data=obj)
         data = response.json()
         if data['artist']:
@@ -126,40 +106,23 @@ class UI(QtWidgets.QMainWindow):
             msg.setWindowTitle("Error")
             msg.setText("Artist not found")
             x = msg.exec_()
-            
+        # clear search bar    
         self.bandSearchLineEdit.setText("")
-
-        screen.close()
+        # remove splash screen if content has loaded
+        splash_screen.close()
 
     def video_search(self, id):
         obj = {"artist_id": id}
+        # call video-search endpoint
         response = requests.post('http://127.0.0.1:5000/video-search', data=obj)
         data = response.json()
         pprint.pprint(data)
         for i, url in enumerate(data["video_urls"]):
             self.video_dict[f"video{i}"].setUrl(QtCore.QUrl(url))
 
-    def city_search_test(self):
-        value = self.mapHomeCityLineEdit.text()
-        obj = {"city_search": value}
-        response = requests.post('http://127.0.0.1:5000/city-search', data=obj)
-        data = response.json()
-        city = data["city"] + ", " + data["state"]
-        coords = data["coords"]
-
-        map = folium.Map(
-            title=city,
-            zoom_start=6,
-            location=coords
-        )
-        data = io.BytesIO()
-        map.save(data, close_file=False)
-        self.mapMapContainer.setHtml(data.getvalue().decode())
-
     def tour_search(self, artist):
-
-        # clear map
-        coordinates = (39.70495909177235, -101.79370097549975)
+        # reset map
+        coordinates = (27.03992362079509, -22.920434372492934)
         self.map = folium.Map(
             title='',
             zoom_start=3,
@@ -168,16 +131,16 @@ class UI(QtWidgets.QMainWindow):
         map_data = io.BytesIO()
         self.map.save(map_data, close_file=False)
         self.mapMapContainer.setHtml(map_data.getvalue().decode())
-
         # get tour dates
         obj = {"artist_search": artist}
+        # call tour-search endpoint
         response = requests.post('http://127.0.0.1:5000/tour-search', data=obj)
         data = response.json()
-
         # add dates to map
         for obj in data["events"]:
             lat = obj['location']['lat']
             lng = obj['location']['lng']
+            # add data to popup dialog in map markers
             iframe = folium.IFrame(f"<section height='100' width='100'><p>{obj['venue']['displayName']}</p><p>{obj['start']['date']}</p><p>{obj['location']['city']}</p><p><a href='{obj['venue']['uri']} target='_blank'>Tickets</a></p></section>")
             popup = folium.Popup(iframe, min_width=300, max_width=300)
             folium.Marker(
@@ -187,33 +150,23 @@ class UI(QtWidgets.QMainWindow):
         map_data = io.BytesIO()
         self.map.save(map_data, close_file=False)
         self.mapMapContainer.setHtml(map_data.getvalue().decode())
-
         # create data strings for display
-        dates = ""
         tourDates = ""
         tourVenues = ""
         tourTickets = ""
         tourCities = ""
-        tourStates = ""
-
         # loop through events to construct data strings
         for date in data["events"]:
-            # dates += f"{date['start']['date']}" + " " + f"{date['venue']['displayName']}" + " " + f"<a href='{date['venue']['uri']}'>{date['venue']['uri']}</a>" + "<br><br>"
             tourDates += f"{date['start']['date']} <br><br>"
             tourVenues += f"{date['venue']['displayName']} <br><br>" 
             tourTickets += f"<a href='{date['venue']['uri']}'>Tickets</a>" + "<br><br>"
             tourCities += f"{date['location']['city']} <br><br>" 
-            # tourStates += f"{date['state']} <br><br>" 
-
-        # update widgets with display data
-        # self.homeTourDatesLabel.setText(dates)
+        # add content to widgets
         self.tourDatesDateListLabel.setText(tourDates)
         self.tourDatesVenueListLabel.setText(tourVenues)
         self.tourDatesTicketListLabel.setText(tourTickets)
-        # self.tourDatesStateListLabel.setText(tourStates)
         self.tourDatesCityListLabel.setText(tourCities)
         
-
     def set_photo(self, img_url):
         data = urllib.request.urlopen(img_url).read()
         pixmap = QPixmap()
@@ -221,22 +174,23 @@ class UI(QtWidgets.QMainWindow):
         self.bandPhotoLabel.setPixmap(pixmap)
         self.bandPhotoLabel.setScaledContents(True)
 
-    def get_artist_name(self, search_string):
-        url = f"http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist={search_string}&api_key={self.API_KEY}&format=json"
-        response = requests.get(url)
-        name = response.json()
-        print("---------------NAME----------------")
-        pprint.pprint(name)
-        return name
+    # def city_search_test(self):
+    #     value = self.mapHomeCityLineEdit.text()
+    #     obj = {"city_search": value}
+    #     response = requests.post('http://127.0.0.1:5000/city-search', data=obj)
+    #     data = response.json()
+    #     city = data["city"] + ", " + data["state"]
+    #     coords = data["coords"]
 
-    def get_artist_bio(self, search_string):
-        url = f"http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist={search_string}&api_key={self.API_KEY}&format=json"
-        response = requests.get(url)
-        pprint.pprint(response.json())
-        bio = response.json()["artist"]["bio"]["summary"]
-        print("---------------BIO----------------")
-        pprint.pprint(bio)
-        return bio
+    #     map = folium.Map(
+    #         title=city,
+    #         zoom_start=6,
+    #         location=coords
+    #     )
+    #     data = io.BytesIO()
+    #     map.save(data, close_file=False)
+    #     self.mapMapContainer.setHtml(data.getvalue().decode())
+
 
 # initialize app
 app = QtWidgets.QApplication(sys.argv)
